58651123c276e39fec9d4f01c87eeb5c5d499e5b
0xff <yaokevin1987@gmail.com>
2018-03-16 09:20:03 +0800

    admin的修改

--- block/admin.py
+++ block/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Block
 
 
-#class BlockAdmin(admin.ModelAdmin)
-#    list_display = ("name","desc","manager")
+class BlockAdmin(admin.ModelAdmin)
+    list_display = ("name","desc","manager")
 
 
-admin.site.register(Block)
+admin.site.register(Block,BlockAdmin)
